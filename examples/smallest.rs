febe708d344473230eef2df8d0754c18662f90cb
Boris Vinogradov <nisembedded@gmail.com>
2025-05-27 12:12:04 +0200

    Add minimal app example

---
+++ examples/smallest.rs
@@ -0,0 +1,27 @@
+#![no_main]
+#![no_std]
+
+use stm32c0xx_hal as hal;
+
+use defmt::info;
+use {defmt_rtt as _, panic_probe as _};
+
+use hal::stm32;
+
+#[rtic::app(device = stm32, peripherals = true)]
+mod app {
+    use super::*;
+
+    #[shared]
+    struct Shared {}
+
+    #[local]
+    struct Local {}
+
+    #[init]
+    fn init(_: init::Context) -> (Shared, Local) {
+        info!("System initialized");
+
+        (Shared {}, Local {})
+    }
+}
